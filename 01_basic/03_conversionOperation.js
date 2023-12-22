b75c736f7156d837f597304226be6a73b3d5ebea
Kishan Kumar Gupta <142384429+kishanKgupta@users.noreply.github.com>
2023-12-22 15:29:29 +0000

    add new code prefix and postfix

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -25,3 +25,44 @@ console.log(typeof stringNum); //string
 // console.log(typeof numvariable);
 // let stringNum = String(numvariable);
 // console.log(typeof stringnum);
+
+console.log("1" + 2);  //12
+console.log(2 + "1");  //21
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 +  "2"); //32
+console.log(1 + "2" + 3);
+console.log(true);  //true
+console.log(+true); //1
+console.log(true+1);  //2
+//-------------------------------
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+//-------------------------------
+
+//postfix
+let x1 = 3;
+const y1 = x++;
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
+
+//prefix
+let x3 = 3;
+const y3 = ++x;
+// x is 4; y is 4
+
+let x4 = 3n;
+const y4 = ++x2;
+// x2 is 4n; y2 is 4n
+
