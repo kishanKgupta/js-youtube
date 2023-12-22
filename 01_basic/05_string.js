4866c417ad349dcf3adf328003501be0c3230044
Kishan Kumar Gupta <142384429+kishanKgupta@users.noreply.github.com>
2023-12-22 19:28:13 +0000

    update

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -2,9 +2,15 @@
 
 //concatenation 
 
-c
-
 const name = "kishan";
 const repoCount = 50;
 
-console.log(name + " " + repoCount);
+console.log(name + " " + repoCount); //old way
+
+//new way "backtick"
+
+console.log(`${name} is good ${repoCount}`); //string interpolation concept
+
+console.log(name.length);
+
+
