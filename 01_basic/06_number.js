869362e3594574a3298d6a3823f9e2f0aed97bc2
Kishan Kumar Gupta <142384429+kishanKgupta@users.noreply.github.com>
2023-12-28 18:16:04 +0000

    update

--- 01_basic/06_number.js
+++ 01_basic/06_number.js
@@ -19,3 +19,8 @@ const min = 10;
 const max = 20;
 
 console.log(Math.floor(Math.random() * (max-min+1)) + min);
+
+// date function.........
+console.log(typeof Date());
+let newDate = new Date();
+console.log(newDate.getDay());
