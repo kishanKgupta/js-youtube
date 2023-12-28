a9207cdc62e99937ddf788afa2cfabd87da28f0f
Kishan Kumar Gupta <142384429+kishanKgupta@users.noreply.github.com>
2023-12-28 17:24:57 +0000

    update

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -4,6 +4,7 @@
 
 const name = "kishan";
 const repoCount = 50;
+console.log(typeof name); //string
 
 console.log(name + " " + repoCount); //old way
 
@@ -12,5 +13,14 @@ console.log(name + " " + repoCount); //old way
 console.log(`${name} is good ${repoCount}`); //string interpolation concept
 
 console.log(name.length);
+
+const gameName = new String('kishan');
+console.log(typeof gameName);
+console.log(name.charAt(2));
+
+//master all property and method of string...
+//if we use property and method that it will not change in actual string...
+
+//convert string into array using split method
 
 
---
+++ 01_basic/06_number.js
@@ -0,0 +1,21 @@
+'use strict';
+const score = 400
+console.log(score);
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+console.log(otherNumber.toPrecision(3)); //important
+//obj.toLocalString
+//obj.MIN_VALUE
+//obj.MAX_VALUE
+
+console.log(Math.random());
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
