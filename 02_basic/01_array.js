8895b3aa5fa31c114b544ddaa0c3461dc219329b
Kishan Kumar Gupta <142384429+kishanKgupta@users.noreply.github.com>
2024-01-07 20:03:34 +0000

    add new feature

--- 02_basic/01_array.js
+++ 02_basic/01_array.js
@@ -1,7 +1,17 @@
 'use strict'
+//() parenthesis 
+//[] brackets
+//{} curly breses
+
 const array = [0,1,2,3,4,5]
 const myHero = ["shaktiman",'naagraaj'];
 const mix = ['A',"ok",123];
+
+//we add diffrente type of value in array...
+for(let i=0;i<array.length;i++)
+{
+    console.log(array[i]);
+}
 
 //shallow copy
 //deep copy
