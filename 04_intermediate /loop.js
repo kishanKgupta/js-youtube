e912bd0233f9a9f2c1e842c88b69ff1416e00dea
Kishan Kumar Gupta <142384429+kishanKgupta@users.noreply.github.com>
2024-01-15 16:53:12 +0000

    update

---
+++ 04_intermediate /loop.js
@@ -0,0 +1,36 @@
+'use script'
+//iterators
+//for loop
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
+
+//for (initialization; condition; update) {
+  // code to be executed
+//}
+
+
+//The for loop is typically used when the number of
+// iterations is known or can be determined in advance.
+
+//The while loop is used when the number of iterations is
+// not known in advance, and the loop continues as long as
+// a specified condition is true
+
+while(condition)
+{
+    //body
+    //update
+}
+
+//In a while loop, the condition is evaluated before the loop
+//body is executed. If the condition is initially false, the
+//loop body may never execute.
+
+do{
+    //body
+}(condition);
+
+//In a do-while loop, the condition is evaluated after the loop body is executed. This guarantees
+//that the loop body will be executed at least once, even if the condition is initially false.
