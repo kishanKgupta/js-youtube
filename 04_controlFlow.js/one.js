36379ae1392982c51c0bc0082f0a71ec6f2f0d36
Kishan Kumar Gupta <142384429+kishanKgupta@users.noreply.github.com>
2024-01-15 14:01:41 +0000

    update

---
+++ 04_controlFlow.js/one.js
@@ -0,0 +1,56 @@
+'use strict'
+//if
+if(condition){
+    //body
+}
+
+//if else
+if(condition)
+{
+   //if condition is true
+}
+else{
+  //if condition is false
+}
+
+//if else ladder
+if(condition)
+{
+   //if condition is true
+}
+else if(condition)
+{
+   //if condition is true
+}
+else{
+   //if condition is false
+}
+
+//switch case
+switch(key){
+    case value:
+        //body
+        break;
+    case value:
+        //body
+        break;
+    default:
+        break;        
+}
+//if not use break statement so where the key is match and afterword all the 
+//case run except default case.....
+
+//truthy and falsy value...
+
+//falsy value
+// false, 0, -0, BigInt 0n, "", null, undefined, 
+
+//truthy false 
+//except falsy value all  are truthy value
+//some extra truthy value...
+// "0" , 'false' , " " , [] ,{} , function(){} -> empty function
+
+//.................................
+//(false == 0) true
+//(false = '') true
+//(0 == '' )true
